6b5d886e923e100456e3a1c7cfcfc54de9405cda
SiunBit <navercloudplatform08_gmail.com>
2024-02-20 14:21:28 +0900

    BingoGame 추가

--- NCP_JavaProject/src/Client/GameClient.java
+++ NCP_JavaProject/src/Client/GameClient.java
@@ -38,5 +38,6 @@ public class GameClient {
 			e.printStackTrace();
 		}
 	}
+	
 
 }
--- NCP_JavaProject/src/Server/GameServer.java
+++ NCP_JavaProject/src/Server/GameServer.java
@@ -1,6 +1,8 @@
 package Server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -8,6 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import BingoGame.Bingo;
 import Client.ClientHandler;
 import liarGame.LiarGame;
 
@@ -19,6 +22,7 @@ public class GameServer {
 		gameClients = new HashMap<>();
 		games = new HashMap<String, Game>();
 		games.put("liar", new LiarGame());
+		games.put("bingo", new Bingo());
 	}
 
 	// 게임서버를 시작하는 메소드
@@ -60,13 +64,42 @@ public class GameServer {
 			System.out.println("게임이름을 잘못입력하셨습니다.");
 		}
 	}
-	/*
-	 * 클라이언트 목록을 출력하려면 List<Socket> clients = gameClients.getOrDefault("liar", new
-	 * ArrayList<>()); System.out.println("Liar Game Clients:"); for (Socket socket
-	 * : clients) System.out.println(socket);
-	 *
-	 * 이런식으로 작성하시면 될거가타여,,,
-	 */
+	
+	
+	public static void main1(String[] args) {
+	    GameServer gameServer = new GameServer();
+	    gameServer.startServer(12345);
+
+	    try {
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	        while (true) {
+	            System.out.println("게임을 선택하세요:");
+	            System.out.println("1. Liar Game");
+	            System.out.println("2. Bingo Game");
+	            System.out.print("번호 입력: ");
+	            String choice = reader.readLine().trim();
+	            
+	            switch (choice) {
+	                case "1":
+	                    // 클라이언트 소켓을 생성하여 Liar Game 시작 메소드에 전달합니다.
+	                    Socket liarSocket = new Socket("127.0.0.1", 12345);
+	                    gameServer.startGame(liarSocket, "liar");
+	                    break;
+	                case "2":
+	                    // 클라이언트 소켓을 생성하여 Bingo Game 시작 메소드에 전달합니다.
+	                    Socket bingoSocket = new Socket("127.0.0.1", 12345);
+	                    gameServer.startGame(bingoSocket, "bingo");
+	                    break;
+	                default:
+	                    System.out.println("잘못된 입력입니다. 다시 시도하세요.");
+	            }
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+
 
 	public static void main(String[] args) {
 		GameServer gameServer = new GameServer();
