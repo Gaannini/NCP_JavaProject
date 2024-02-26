91053ec37344870817b22d22b20e060611d4dc1b
cyj127 <rkdgudtjd112@naver.com>
2024-02-26 12:52:37 +0900

    불필요한 Omok 클래스 삭제, 수정된 Omok 클래스 등록.

--- NCP_JavaProject/src/omokGame/OmokClient.java
+++ NCP_JavaProject/src/omokGame/OmokClient.java
@@ -4,137 +4,346 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-import javax.naming.InitialContext;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 
-import Server.Game;
+public class OmokClient extends JFrame {
 
-public class OmokClient extends JFrame implements Game {
-	JPanel omokJPanel;
-	JLabel omokJLabel;
-	JButton omokJButton;
-//	OmokGame omokGame = new OmokGame();
+    //서버 통신 Ip, port
+    String server = "127.0.0.1";
+    int serverPort = 99999;
 
-	private PrintWriter writer;
-	private BufferedReader reader;
-	private Socket socket;
-	ImageIcon img = new ImageIcon("images//empty.png");
-	ImageIcon white = new ImageIcon("images//white.png");
-	ImageIcon black = new ImageIcon("images//black.png");
-	ImageIcon turn = black;
+    //
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+    private clientInfo CI;
 
-	@Override
-	public void start(Socket socket) {
-		// TODO Auto-generated method stub
 
-	}
 
-	public OmokClient(Socket socket) {
-		this.socket = socket; // 소켓을 받아서 멤버 변수에 할당
-		listener listener = new listener();
-		listener.init();
-		setting();
-		batch();
-		new listener();
+    //화면
+
+    private ImageIcon img = new ImageIcon("images//empty.png");
+    private ImageIcon white = new ImageIcon("images//white.png");
+    private ImageIcon black = new ImageIcon("images//black.png");
+    private ImageIcon turn = black;
+
+
+    GoEgg[][] goEgg = new GoEgg[26][];
+
+
+
+//    //main
+//    public static void main(String[] args) {
+//       try {
+//		new OmokClient(socket);
+//	} catch (Exception e) {
+//		// TODO Auto-generated catch block
+//		e.printStackTrace();
+//	}
+//    }
+
+    //생성자
+    public OmokClient(Socket socket) {
+//		
+		init(); //초기화 메서
 		setVisible(true);
-		new GoEgg(EXIT_ON_CLOSE, ABORT, img);
-//		omokGame.startgame();
-	}
-
-	//
-	
-	private void batch() {
-		// add
-
-	}
-
-	private  void setting() {
-		// 크기, 타이틀, 사이즈 등등
-	
-
-		setTitle("===오목게임===");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		Container c = getContentPane();
-		c.setLayout(new GridLayout(26, 26));
-	}
-
-	// 클릭했을때 흰돌놓을지 검은돌놓을지
-	private class listener extends OmokGame implements ActionListener {
-		GoEgg goEgg[][];
-		private Container c;
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			omokGame.GoEgg wi = (omokGame.GoEgg) e.getSource();
-			if (turn == white) {
-				wi.setIcon(white);
-				wi.state = "W";
-				turn = black;
-
-			} else {
-				wi.setIcon(black);
-				wi.state = "B";
-				turn = white;
-			}
-			checkWin(wi);
-			wi.removeActionListener(this);
-		}
-
-		private  void setting() {
-			// 크기, 타이틀, 사이즈 등등
-			setTitle("===오목게임===");
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-			c = getContentPane();
-			c.setLayout(new GridLayout(26, 26));
-		}
 		
-		//착수한 돌을 배열에 저장
-		private void init (){
-			listener goAction = new listener();
-			
-			for (int i = 0; i < 26; i++) {
-				goEgg[i] = new GoEgg[26];
-				for (int j = 0; j < 26; j++) {
-					goEgg[i][j] = new GoEgg(i, j, img);
-					c.add(goEgg[i][j]);
-					goEgg[i][j].addActionListener(goAction);
-					goEgg[i][j].setBorderPainted(false);
-				}
-			}
+		this.socket = socket;
 
-			setSize(1000, 1000);
-			setVisible(true);
-			// 바둑알 생성
 
-		}
-		
-		//오목 좌표 생성
-		class GoEgg extends JButton {
-			int x;
-			int y;
-			String state;
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+//                sendExitMsg();
+//                disconnectServer(); // 서버와의 연결 끊기
+            }
+        });
+    }
 
-			public GoEgg(int x, int y, ImageIcon image) {
-				super(image);
-				this.x = x;
-				this.y = y;
-				state = "N";
-			}
 
-		}
-	}
+    // 서버에서 상대방  받기위한 스레드
+    public class clientInfo extends Thread {
+        private Socket socket;
+        private BufferedReader reader;
+        public int[][] goEgg;
+
+        public clientInfo(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new PrintWriter(socket.getOutputStream(), true);
+
+//                while ((acceptchat = reader.readLine()) != null) {
+//                    chatreset(acceptchat);
+//                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //화면 생성
+    private void init() {
+
+        setTitle("===오목 게임===");
+        //윈도우 창 종료시 프로세스 종료
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container c = getContentPane();
+        c.setLayout(new GridLayout(26, 26));
+
+       ActionListener goAction =  new myActionListener();
+        for (int i = 0; i < 26; i++) {
+            goEgg[i] = new GoEgg[26];
+            for (int j = 0; j < 26; j++) {
+                goEgg[i][j] = new GoEgg(i, j, img);
+                c.add(goEgg[i][j]);
+                goEgg[i][j].addActionListener(goAction);
+                goEgg[i][j].setBorderPainted(false);
+            }
+        }
+
+        setSize(1000, 1000);
+        setVisible(true);
+
+    }
+
+    //게임 이벤트 처리
+        class myActionListener implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GoEgg wi = (GoEgg) e.getSource();
+                if (turn == white) {
+                    wi.setIcon(white);
+                    wi.state = "W";
+                    turn = black;
+
+                } else {
+                    wi.setIcon(black);
+                    wi.state = "B";
+                    turn = white;
+                }
+                checkWin(wi);
+                wi.removeActionListener(this);
+            }
+          //승패 판단 
+        	public void checkWin(GoEgg e) {
+        		int checkx = e.x;
+        		int checky = e.y;
+        		int count = 0;
+        		while (checky >= 0 && goEgg[checkx][checky].state.equals(e.state)) {
+        			checky -= 1;
+        		}
+        		checky += 1;
+        		while (checky < 26 && goEgg[checkx][checky].state.equals(e.state)) {
+        			checky += 1;
+        			count++;
+        		}
+        		if (count == 5) {
+        			if (e.state.equals("B")) {
+        				JOptionPane.showMessageDialog(null, "흑돌이 승리하였습니다.", "게임 승리", JOptionPane.QUESTION_MESSAGE);
+        				int result = JOptionPane.showConfirmDialog(null,
+        				        "게임을 종료합니다.",
+        				        "Confirm Quit", JOptionPane.YES_OPTION);
+        				if (result == JOptionPane.YES_OPTION) System.exit(0);
+        				
+        				
+        			} else {
+        				JOptionPane.showMessageDialog(null, "백돌이 승리하였습니다.", "게임 승리", JOptionPane.QUESTION_MESSAGE);
+        				int result = JOptionPane.showConfirmDialog(null,
+        				        "게임을 종료합니다.",
+        				        "Confirm Quit", JOptionPane.YES_OPTION);
+        				if (result == JOptionPane.YES_OPTION) System.exit(0);
+        			
+        			}
+
+        		}
+        		checkx = e.x;
+        		checky = e.y;
+        		count = 0;
+
+        		while (checkx >= 0 && goEgg[checkx][checky].state.equals(e.state)) {
+        			checkx -= 1;
+        		}
+        		checkx += 1;
+        		while (checkx < 26 && goEgg[checkx][checky].state.equals(e.state)) {
+        			checkx += 1;
+        			count++;
+        		}
+        		if (count == 5) {
+        			if (e.state.equals("B")) {
+        				JOptionPane.showMessageDialog(null, "흑돌이 승리하였습니다.", "게임 승리", JOptionPane.QUESTION_MESSAGE);
+        				int result = JOptionPane.showConfirmDialog(null,
+        				        "게임을 종료합니다.",
+        				        "Confirm Quit", JOptionPane.YES_OPTION);
+        				if (result == JOptionPane.YES_OPTION) System.exit(0);
+        				
+        				
+        			} else {
+        				JOptionPane.showMessageDialog(null, "백돌이 승리하였습니다.", "게임 승리", JOptionPane.QUESTION_MESSAGE);
+        				int result = JOptionPane.showConfirmDialog(null,
+        				        "게임을 종료합니다.",
+        				        "Confirm Quit", JOptionPane.YES_OPTION);
+        				if (result == JOptionPane.YES_OPTION) System.exit(0);
+        			
+        			}
+
+        		}
+        		////////////////////
+        		checkx = e.x;
+        		checky = e.y;
+        		count = 0;
+
+        		while (checkx >= 0 && checky >= 0 && goEgg[checkx][checky].state.equals(e.state)) {
+        			checkx -= 1;
+        			checky -= 1;
+        		}
+        		checkx += 1;
+        		checky += 1;
+        		while (checkx < 26 && checky < 26 && goEgg[checkx][checky].state.equals(e.state)) {
+        			checkx += 1;
+        			checky += 1;
+        			count++;
+        		}
+        		if (count == 5) {
+        			if (e.state.equals("B")) {
+        				JOptionPane.showMessageDialog(null, "흑돌이 승리하였습니다.", "게임 승리", JOptionPane.QUESTION_MESSAGE);
+        				int result = JOptionPane.showConfirmDialog(null,
+        				        "게임을 종료합니다.",
+        				        "Confirm Quit", JOptionPane.YES_OPTION);
+        				if (result == JOptionPane.YES_OPTION) System.exit(0);
+        				
+        				
+        			} else {
+        				JOptionPane.showMessageDialog(null, "백돌이 승리하였습니다.", "게임 승리", JOptionPane.QUESTION_MESSAGE);
+        				int result = JOptionPane.showConfirmDialog(null,
+        				        "게임을 종료합니다.",
+        				        "Confirm Quit", JOptionPane.YES_OPTION);
+        				if (result == JOptionPane.YES_OPTION) System.exit(0);
+        			
+        			}
+
+        		}
+        		checkx = e.x;
+        		checky = e.y;
+        		count = 0;
+
+        		while (checkx >= 0 && checky < 26 && goEgg[checkx][checky].state.equals(e.state)) {
+        			checkx -= 1;
+        			checky += 1;
+        		}
+        		checkx += 1;
+        		checky -= 1;
+        		while (checkx < 26 && checky >= 0 && goEgg[checkx][checky].state.equals(e.state)) {
+        			checkx += 1;
+        			checky -= 1;
+        			count++;
+        		}
+        		if (count == 5) {
+        			if (e.state.equals("B")) {
+        				JOptionPane.showMessageDialog(null, "흑돌이 승리하였습니다.", "게임 승리", JOptionPane.QUESTION_MESSAGE);
+        				int result = JOptionPane.showConfirmDialog(null,
+        				        "게임을 종료합니다.",
+        				        "Confirm Quit", JOptionPane.YES_OPTION);
+        				if (result == JOptionPane.YES_OPTION) System.exit(0);
+        				
+        				
+        			} else {
+        				JOptionPane.showMessageDialog(null, "백돌이 승리하였습니다.", "게임 승리", JOptionPane.QUESTION_MESSAGE);
+        				int result = JOptionPane.showConfirmDialog(null,
+        				        "게임을 종료합니다.",
+        				        "Confirm Quit", JOptionPane.YES_OPTION);
+        				if (result == JOptionPane.YES_OPTION) System.exit(0);
+        			
+        			}
+
+        		}
+        	}
+        		
+            // 서버연결메소드
+            // 접속 시 서버 연결 메서드.
+            private void connectServer() {
+                try {
+                    socket = new Socket(server, serverPort);
+                    CI = new OmokClient.clientInfo(socket);
+                    CI.start();
+                } catch (Exception e) {
+                    System.out.println(server + ": 서버 연결 실패");
+                }
+            }
+
+
+
+
+            // 서버에서 메세지 받기위한 스레드
+            public class clientInfo extends Thread {
+                private Socket socket;
+                private BufferedReader reader;
+                public String clientId;
+                public String gamename;
+
+                public clientInfo(Socket socket) {
+                    this.socket = socket;
+                }
+
+                @Override
+                public void run() {
+                    try {
+                        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        writer = new PrintWriter(socket.getOutputStream(), true);
+
+//                        while ((acceptchat = reader.readLine()) != null) {
+//                            chatreset(acceptchat);
+//                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+                private void sendExitMsg() {
+                    try {
+                        writer = new PrintWriter(socket.getOutputStream(), true);
+                        writer.println("EXIT&퇴장");
+                        writer.flush();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
+    }
+
+class GoEgg extends JButton {
+    int x;
+    int y;
+    String state;
+
+    public GoEgg(int x, int y, ImageIcon image) {
+        super(image);
+        this.x = x;
+        this.y = y;
+        state = "N";
+    }
+
 }
-
-
-
-// 바둑판 위치 서버에 전달하는 메소드
--- NCP_JavaProject/src/omokGame/OmokServer.java
+++ NCP_JavaProject/src/omokGame/OmokServer.java
@@ -1,209 +1,61 @@
 package omokGame;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import javax.swing.JOptionPane;
+import java.util.HashMap;
+import java.util.Map;
 
 import Server.Game;
 
-public class OmokServer  extends OmokGame implements Game {
-	
+public class OmokServer implements Game {
 
 
-	private Socket socket;
-	
-	  public static void main(String[] args) {
-	        final int PORT = 7878; // 사용할 포트 번호
+    private Map<int[][], String> playerEgg; //돌변경 위치, 클라이언트1/2
+    private static ServerSocket serverSocket; // 서버 소켓.
+    private static Socket clientsocket; // 클라이언트가 접속하면 새로 만드는 소켓
 
-	        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-	            System.out.println("서버가 포트 " + PORT + "에서 실행 중...");
+    //생성자
+    public OmokServer(){
 
-	            // 클라이언트의 연결을 수신하고, 클라이언트와 통신할 소켓을 얻습니다.
-	            Socket clientSocket = serverSocket.accept();
-	            System.out.println("클라이언트가 연결되었습니다.");
+    }
 
-	            // 클라이언트와 데이터를 주고받을 입력 및 출력 스트림을 얻습니다.
-	            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-	            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    public static void main(String[] args) {
+        OmokServer.startServer(99999);
+    }
+    public static void startServer(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("서버 시작. port : " + port + "...");
 
-	            // 클라이언트에게 메시지를 전송합니다.
-	            out.println("서버로부터의 메시지: 연결이 성공적으로 수립되었습니다.");
+            while (true) {
+                clientsocket = serverSocket.accept(); // 서버의 접속을 대기중
+                System.out.println("클라이언트 연결 : " + clientsocket);
 
-	            // 클라이언트로부터 메시지를 수신하고 출력합니다.
-	            String clientMessage = in.readLine();
-	            System.out.println("클라이언트로부터의 메시지: " + clientMessage);
 
-	            // 클라이언트와 통신을 종료합니다.
-	            clientSocket.close();
-	        } catch (IOException e) {
-	            System.err.println("서버 오류: " + e.getMessage());
-	        }
-	    }
-	//OmokGame omokGame = new OmokGame();
+//                // 클라이언트 처리를 위한 쓰레드 시작
+//                clientInfo CI = new clientInfo(clientsocket);
+//                CI.start();
+            }
+        } catch (Exception e) {
+            System.out.println("게임서버 연결안됨.");
+        }
+    }
+
+    //client 받을것들, 돌위치 변경,, 전달 할것들..?
+    public class clientInfo extends Thread {
+
+        private final Socket socket;
+
+        public clientInfo(Socket socket) {
+            this.socket = socket;
+        }
+
+    }
+
 	@Override
 	public void start(Socket socket) {
-		this.socket = socket;
-		new clientInfo(socket).start(); // 클라이언트 연결을 처리하는 쓰레드 시작
-		
-		//omokGame.startgame();
-	}
-
-	public class clientInfo extends Thread {
-		private Socket socket; // 클라이언트 소켓을 받아서 사용하는 변수
-		public PrintWriter writer; // 쓰기 버퍼.
-		private BufferedReader reader; // 읽기 버퍼.
-		public String omokMsg; // 클라이언트 아이디를 담는 변수.
-		public String gamename; // 클라이언트가 선택한 게임 이름
-
-		public clientInfo(Socket socket) {
-			this.socket = socket;
-		}
-
-		@Override
-		public void run() {
-			try {
-				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				writer = new PrintWriter(socket.getOutputStream(), true);
-
-				String clientMsg;
-				while ((clientMsg = reader.readLine()) != null) {
-					String[] parsedMsg = clientMsg.split("&");
-					// Client Thread에서 동작하는 프로토콜
-					handleProtocol(parsedMsg);
-
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		private void handleProtocol(String[] parsedMsg) {
-			if (parsedMsg.length >= 2) {
-				String protocol = parsedMsg[0];
-				String data = parsedMsg[1];
-
-				switch (protocol) {
-				case "omok":
-					omokMsg = data;
-					// clientIds.put(socket, clientId);
-					System.out.println(omokMsg + "확인");
-					break;
-
-				}
-			}
-		}
-			//오목 승리 조건 체크 메서드
-			public void checkWin(GoEgg e) {
-				//오목 조건 체크
-				int checkx = e.x;
-				int checky = e.y;
-				int count = 0;
-				//세로 방향 체크
-				while (checky >= 0 && goEgg[checkx][checky].state.equals(e.state)) {
-					checky -= 1;
-				}
-				checky += 1;
-				while (checky < 26 && goEgg[checkx][checky].state.equals(e.state)) {
-					checky += 1;
-					count++;
-				}
-				if (count == 5) {
-					if (e.state.equals("B")) {
-						JOptionPane.showMessageDialog(null,  "흑돌이 승리했습니다.","게임종료", JOptionPane.QUESTION_MESSAGE);
-					} else {
-						JOptionPane.showMessageDialog(null, "백돌이 승리했습니다.","게임종료", JOptionPane.QUESTION_MESSAGE);
-					}
-
-				}
-				/////////////////////////////////
-				checkx = e.x;
-				checky = e.y;
-				count = 0;
-				
-				//가로 방향 체크
-				while (checkx >= 0 && goEgg[checkx][checky].state.equals(e.state)) {
-					checkx -= 1;
-				}
-				checkx += 1;
-				while (checkx < 26 && goEgg[checkx][checky].state.equals(e.state)) {
-					checkx += 1;
-					count++;
-				}
-				if (count == 5) {
-					if (e.state.equals("B")) {
-						JOptionPane.showMessageDialog(null, "흑돌이 승리했습니다.","게임종료", JOptionPane.QUESTION_MESSAGE);
-					} else {
-						JOptionPane.showMessageDialog(null, "백돌이 승리했습니다.","게임종료", JOptionPane.QUESTION_MESSAGE);
-					}
-				}
-				////////////////////
-				checkx = e.x;
-				checky = e.y;
-				count = 0;
-
-				//↖방향 체크
-				while (checkx >= 0 && checky >= 0 && goEgg[checkx][checky].state.equals(e.state)) {
-					checkx -= 1;
-					checky -= 1;
-				}
-				checkx += 1;
-				checky += 1;
-				while (checkx < 26 && checky < 26 && goEgg[checkx][checky].state.equals(e.state)) {
-					checkx += 1;
-					checky += 1;
-					count++;
-				}
-				if (count == 5) {
-					if (e.state.equals("B")) {
-						JOptionPane.showMessageDialog(null, "흑돌이 승리했습니다.","게임종료", JOptionPane.QUESTION_MESSAGE);
-					} else {
-						JOptionPane.showMessageDialog(null, "백돌이 승리했습니다.","게임종료", JOptionPane.QUESTION_MESSAGE);
-					}
-				}
-				//		//////////////////
-				checkx = e.x;
-				checky = e.y;
-				count = 0;
-
-				//↗↙방향 체크
-				while (checkx >= 0 && checky < 26 && goEgg[checkx][checky].state.equals(e.state)) {
-					checkx -= 1;
-					checky += 1;
-				}
-				checkx += 1;
-				checky -= 1;
-				while (checkx < 26 && checky >= 0 && goEgg[checkx][checky].state.equals(e.state)) {
-					checkx += 1;
-					checky -= 1;
-					count++;
-				}
-
-				if (count == 5) {
-					if (e.state.equals("B")) {
-						JOptionPane.showMessageDialog(null, "흑돌이 승리했습니다.","게임종료", JOptionPane.QUESTION_MESSAGE);
-					} else {
-						JOptionPane.showMessageDialog(null, "백돌이 승리했습니다.","게임종료", JOptionPane.QUESTION_MESSAGE);
-					}
-				}
-
-			}
-			
-			
-			
-		}
-
-		
-	
-
-	public void startgame() {
-		new OmokGame();
 		// TODO Auto-generated method stub
 		
 	}
 
-}
+    }
